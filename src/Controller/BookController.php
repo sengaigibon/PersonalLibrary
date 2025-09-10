32714770be135fe0d9946a22f3f1d96227810a72
El Presidente <javier.caballero@coxautoinc.com>
2025-09-10 12:59:52 +0200

    Update: Remove index action as is performed somewhere else

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -11,17 +11,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/book')]
+#[Route('/books')]
 final class BookController extends AbstractController
 {
-    #[Route(name: 'app_book_index', methods: ['GET'])]
-    public function index(BookRepository $bookRepository): Response
-    {
-        return $this->render('book/index.html.twig', [
-            'books' => $bookRepository->findAll(),
-        ]);
-    }
-
     #[Route('/new', name: 'app_book_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
