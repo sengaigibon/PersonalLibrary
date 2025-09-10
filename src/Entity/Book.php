6ed98e926b0ff8db25d76bfaba6c6e4cb5519bd7
El Presidente <javier.caballero@coxautoinc.com>
2025-09-10 11:17:10 +0200

    Add: Entities for book and logs

---
+++ src/Entity/Book.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 13, nullable: true)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $pages = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTime $purchaseDate = null;
+
+    /**
+     * @var Collection<int, ReadLog>
+     */
+    #[ORM\OneToMany(targetEntity: ReadLog::class, mappedBy: 'book')]
+    private Collection $readLogs;
+
+    public function __construct()
+    {
+        $this->readLogs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(?string $isbn): static
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getPages(): ?int
+    {
+        return $this->pages;
+    }
+
+    public function setPages(?int $pages): static
+    {
+        $this->pages = $pages;
+
+        return $this;
+    }
+
+    public function getPurchaseDate(): ?\DateTime
+    {
+        return $this->purchaseDate;
+    }
+
+    public function setPurchaseDate(?\DateTime $purchaseDate): static
+    {
+        $this->purchaseDate = $purchaseDate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ReadLog>
+     */
+    public function getReadLogs(): Collection
+    {
+        return $this->readLogs;
+    }
+
+    public function addReadLog(ReadLog $readLog): static
+    {
+        if (!$this->readLogs->contains($readLog)) {
+            $this->readLogs->add($readLog);
+            $readLog->setBook($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReadLog(ReadLog $readLog): static
+    {
+        if ($this->readLogs->removeElement($readLog)) {
+            // set the owning side to null (unless already changed)
+            if ($readLog->getBook() === $this) {
+                $readLog->setBook(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/ReadLog.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReadLogRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReadLogRepository::class)]
+class ReadLog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTime $startDate = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $finishDate = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $rating = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $notes = null;
+
+    #[ORM\ManyToOne(inversedBy: 'readLogs')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Book $book = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartDate(): ?\DateTime
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(?\DateTime $startDate): static
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getFinishDate(): ?\DateTime
+    {
+        return $this->finishDate;
+    }
+
+    public function setFinishDate(\DateTime $finishDate): static
+    {
+        $this->finishDate = $finishDate;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?int $rating): static
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getNotes(): ?string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(?string $notes): static
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    public function getBook(): ?Book
+    {
+        return $this->book;
+    }
+
+    public function setBook(?Book $book): static
+    {
+        $this->book = $book;
+
+        return $this;
+    }
+}
